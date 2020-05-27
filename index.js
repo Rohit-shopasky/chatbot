1a5c42a76f7c5686d0eb956fc33f87500e75e1e1
Rohit-shopasky <>
2020-05-27 20:51:30 +0530

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const verifyWebhook = require('./verify-webhook');
+
+
+app.get('/', verifyWebhook);
+
+app.listen(5000, () => console.log('Express server is listening on port 5000'));
